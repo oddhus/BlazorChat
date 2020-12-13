42b176523e198d9bcdee7e799af9c62388c1d09f
Oddmund Huseby <huseby.oddmund@gmail.com>
2020-12-13 20:13:23 +0100

    user dtos

--- Client/ViewModels/ProfileViewModel.cs
+++ Client/ViewModels/ProfileViewModel.cs
@@ -8,14 +8,16 @@ namespace BlazorChat.ViewModels
 
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public string EmailAddress { get; set; }
+        public string Address { get; set; }
 
-        public static implicit operator ProfileViewModel()
+        public static implicit operator ProfileViewModel(UserReadDto user)
         {
             return new ProfileViewModel
             {
-
-            }
+                Firstname = user.Firstname,
+                Lastname = user.Lastname,
+                Address = user.Address,
+            };
         }
     }
 }
